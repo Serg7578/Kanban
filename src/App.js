664c0b50ed6f856d788a15c2379f48f55e6b6e00
Sergei <92303114+Serg7578@users.noreply.github.com>
2023-10-13 19:28:53 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Header from './components/header/Header'; 
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
+
+function App() {
+	
+	const initialState = JSON.parse(window.localStorage.getItem('tasks')) || []
+    const [tasks, setTasks] = useState(initialState)
+
+	useEffect(() => {
+		window.localStorage.setItem('tasks', JSON.stringify(tasks))
+	}, [tasks]) 
+    
+  return (
+    <div className='wrapper'>
+        <Header/>
+		    <Main tasks={tasks} setTasks={setTasks} />
+		    <Footer tasks={tasks} />
+    </div>
+  );
+}
+
+export default App;
